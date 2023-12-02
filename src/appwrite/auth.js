0cb844b6a7ab950da2fa4ac070b79a011f801adc
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-03 00:22:11 +0530

    Auth.js

---
+++ src/appwrite/auth.js
@@ -0,0 +1,16 @@
+import conf from "../config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+         this.account = new Account(this.client);   
+    }   
+}
+
+const authService = new AuthService();
+export default AuthService
--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,9 @@
 const conf={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteUrl:String(import.meta.env.VITE_DATABASE_URL),
-    appwriteUrl:String(import.meta.env.VITE_COLLECTION_URL),
-    appwriteUrl:String(import.meta.env.VITE_BUCKET_URL)    
+    appwriteDatabase:String(import.meta.env.VITE_DATABASE_URL),
+    appwriteCollection:String(import.meta.env.VITE_COLLECTION_URL),
+    appwriteBucket:String(import.meta.env.VITE_BUCKET_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID)    
+}    
 }
 export default conf
