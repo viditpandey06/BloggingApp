44ef7d0c9395e813ce77bed296e0127d5cabd99e
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-03 21:51:03 +0530

    Backend

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -3,14 +3,59 @@ import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
     client = new Client();
-    account
+    account;
     constructor(){
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId)
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId)
          this.account = new Account(this.client);   
     }   
+    async createAccount ({email, password, name}){
+        try {
+       const userAccount =     await this.account.create(ID.unique,email,name)
+       if (userAccount) {
+        //call another method
+        this.login({email,password})
+       } else {
+            return userAccount;          
+       }    
+    } catch (error) {
+            console.error();
+            throw error;
+        }
+    }
+    async login(email,password){
+        try {
+          return  await this. account. createEmailSession(email, password)
+        } catch (error) {
+            console.error();
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try {
+           const currentUser= await this.account.get();
+           if(currentUser){
+               return currentUser
+           }
+           else{
+            return null;
+           }
+        } catch (error) {
+            console.error();
+            throw error;
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await  this.account.deleteSession();
+        } catch (error) {
+            console.error();
+            throw error;
+        }
+    }
 }
 
 const authService = new AuthService();
-export default AuthService
+export default authService
