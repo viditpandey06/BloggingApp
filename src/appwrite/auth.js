82139b2160ad0d8cd835cdb7ff64842cf34d1cb1
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-03 21:25:37 +0530

    Authentication Service Setup

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from "../config";
+import config from "../config";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
-const conf={
+const config={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
     appwriteDatabase:String(import.meta.env.VITE_DATABASE_URL),
     appwriteCollection:String(import.meta.env.VITE_COLLECTION_URL),
     appwriteBucket:String(import.meta.env.VITE_BUCKET_URL),
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID)    
 }    
-}
-export default conf
+
+export default config
