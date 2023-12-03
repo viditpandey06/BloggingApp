d40ef589c49bf3b7db0228c8ae1fb18897e35c8f
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-03 23:36:34 +0530

    OTP login

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,6 +2,7 @@ import config from "../config";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
+
     client = new Client();
     account;
     constructor(){
@@ -12,7 +13,7 @@ export class AuthService{
     }   
     async createAccount ({email, password, name}){
         try {
-       const userAccount =     await this.account.create(ID.unique,email,name)
+       const userAccount =     await this.account.create(ID.unique(),email,name)
        if (userAccount) {
         //call another method
         this.login({email,password})
@@ -24,6 +25,24 @@ export class AuthService{
             throw error;
         }
     }
+    async createMessageLoginUser (phone_no){
+      try {
+        const userAccount = await this.account.createPhoneSession(ID.unique,String(phone_no))
+        const userId = userAccount.userId;
+        return userAccount;
+      } catch (error) {
+       console.error();
+        throw error
+      }
+    }
+    async MessageLogin(userId,code){
+        try {
+          return await this.account.updatePhoneSession(userId,String(code));
+        } catch (error) {
+          console.error();
+          throw error;
+        }
+    } 
     async login(email,password){
         try {
           return  await this. account. createEmailSession(email, password)
