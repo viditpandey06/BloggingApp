ba9d525f21219b484fddede58b5d1a73354135f2
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-03 23:03:02 +0530

    Update databaseConfig.js

--- src/appwrite/databaseConfig.js
+++ src/appwrite/databaseConfig.js
@@ -1,2 +1,64 @@
 import config from "../config/config";
-import{Client,ID,Databases,Storage,Query} from "appwrite";
+import { Client, ID, Databases, Storage, Query, Account } from "appwrite";
+export class databaseService {
+  client = new Client();
+  databases;
+  storage;
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.storage = new Storage(this.client);
+  }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      const post = await this.databases(
+        config.appwriteDatabase,
+        config.appwriteCollection,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+      if (post) {
+        return post;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error();
+      throw error;
+    }
+  }
+  t;
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      const update = await this.databases.updateDocument(
+        config.appwriteDatabase,
+        config.appwriteCollection,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+      if (update) {
+        return update;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error();
+      throw error;
+    }
+  }
+}
+const service = new databaseService();
+export default service;
