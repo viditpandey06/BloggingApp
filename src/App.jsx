60dd6f1597766f2043a6b0f2563e27a776ef8fc2
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-13 14:48:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from 'react'
-import { useDispatch } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
-import {} from "./store"
+import {login,logout} from "./store/authSlice"
+import { Footer,Header } from "./components/index";
+import { Outlet } from "react-router-dom";
+//import {Outlet} from 'react-dom'
 function App() {
   const [loading,setLoading]=useState(true)
-  const dispath = useDispatch()
-useEffect(()=>{
-  authService.getCurrentUser()
-  .then((userData)=>{
-     if
-  })
-  .catch()
-  .finally()
-},[])
-  //console.log(import.meta.env.VITE_APP_APPWRITE_URL);
-  return (
-    <>
-      <h1>Vidit</h1>
-    </>
-  )
+  const dispatch  = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch(()=>{
+        console.log("Error occured");
+    })
+    .finally(()=> setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
