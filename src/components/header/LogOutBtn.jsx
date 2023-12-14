55c897af830c12b539bbd5fb7aaa4f97b30c5254
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-15 00:08:31 +0530

    Components

---
+++ src/components/header/LogOutBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {useDispatch} from 'react-dom'
+import authService from '../../appwrite/databaseConfig'
+import {logout} from '../../store/authSlice'
+function LogOutBtn() {
+  
+}
+
+export default LogOutBtn
