ebb698c44eadf79fc2531f1a00f8e7f569a8993d
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-17 00:18:04 +0530

    Update LogOutBtn.jsx

--- src/components/header/LogOutBtn.jsx
+++ src/components/header/LogOutBtn.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import {useDispatch} from 'react-dom'
+import { useDispatch } from 'react-dom'
 import authService from '../../appwrite/databaseConfig'
-import {logout} from '../../store/authSlice'
+import { logout } from '../../store/authSlice'
 function LogOutBtn() {
-  
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout()
+            .then(() => {
+                dispatch(logout())
+            })
+    }
+    return (<button className='inline-block px-6 py-2 duration-200
+ hover: bg-blue-100 rounded-full'
+    >Logout</button>)
 }
 
 export default LogOutBtn
