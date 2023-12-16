0afbbce0950223e5ea60aac0911e7fb55572a963
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-17 00:18:01 +0530

    Update index.js

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,4 +1,8 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import Container from "./container/container"
+import Logo from "./Logo"
+import LogOutBtn from "../header/LogOutBtn";
 
-export  {Header,Footer};
+
+export  {Header,Footer,Container,Logo,LogOutBtn};
