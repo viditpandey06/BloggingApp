181f35ce598410e5f91c09427a428163ae1190b4
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-17 00:17:59 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import { Container, Logo, LogOutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+
   return (
     <div>Header</div>
   )
